9cd9d045735071290c827169f70f17a8ca54b01e
burkaloo <burkedev@gmail.com>
2023-01-23 14:34:34 +0800

    simple form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h3>How to Use</h3>
+    <p>Input cellphone numbers only</p>
+    <p>Numbers must be 10 digit format starting with 9 and no spaces or other special characters</p>
+    <p>Can get up to 50 numbers at once</p>
+    <p>seperate each number with a comma(,) and no space</p>
+    <p>this form has no input validation. Please make sure your input vales and format is correct</p>
+  </div>
+  <div>
+    <textarea name="" id="" cols="60" rows="5" v-model="numbers" maxlength="600" placeholder="9170001122,9183334455,9060..."></textarea>
+  </div>
+  <br>
+  <div>
+    <button :disabled="gettingscores" @click="getscores">Get Score</button>
+    <span v-if="gettingscores"> Getting Scores....</span>
+  </div>
+  <br>
+  <table v-if="results.length > 0" :style="{width: '200px'}">
+    <thead>
+      <tr>
+        <td>Number</td>
+        <td>Score</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="obj in results" :key="obj.number">
+        <td>{{ obj.number }}</td>
+        <td>{{ obj.score }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const axios = require('axios');
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      results: [],
+      gettingscores: false,
+      numbers: "",
+      errormsg:""
+    }
+  },
+  methods:{
+    getscores(){
+      this.results = []
+      this.gettingscores = true
+      axios.post("https://utils.polka.ph/inquiro/inquiro.php", {
+        numbers: this.numbers,
+      })
+      .then((res) => {
+        if(res.data.status == "success"){
+          this.results = res.data.response
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => {
+        this.gettingscores = false
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
